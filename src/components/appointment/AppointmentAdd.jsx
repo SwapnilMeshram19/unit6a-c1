01e452d0c54be1c082f5f4b0fa3ab0c21fc8b4c1
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-07-18 20:27:27 +0530

    .

---
+++ src/components/appointment/AppointmentAdd.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AppointmentAdd =()=>{
+
+    return (<div>
+        
+    </div>)
+}
+
+export default AppointmentAdd;
---
+++ src/components/appointment/AppointmentId.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AppointementId =()=>{
+
+    return (<div>
+        
+    </div>)
+}
+
+export default AppointementId;
